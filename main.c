299e8e9db69c03c5eb5408e6cdfe792f2a8068ad
Abram Darmaputra <71720135+abramdarmaputra@users.noreply.github.com>
2023-05-23 20:22:27 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+#include "DrvSYS.h"
+#include "DrvGPIO.h"
+#include "LCD_Driver.h"
+#include "ScanKey.h"
+#include "Seven_Segment.h"
+#include <String.h>
+
+int j, k, i, angka, sat, pul, rat, rib, angka1, sat1, pul1, rat1, rib1, nilai;
+int waktu = 300;
+int a = 0;
+int life = 5;
+int adc;
+int tegangan;
+unsigned char temp;
+
 void Init();
 int main(void)
 {
